15ba9e5284699f9a0b2b39f30b9bb0d23f55a2c5
Josh Priestley <josh@joshpriestley.co.uk>
2015-07-22 22:42:06 +0000

    Moving custom grafana config out into editable file
    Adding nginx proxy for grafana and graphite

--- install_grafana.sh
+++ install_grafana.sh
@@ -6,3 +6,11 @@ curl https://packagecloud.io/gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install grafana
 
+grep '{' ./grafana/custom.ini > /dev/null
+if [[ $? != 1 ]]; then
+	echo "Looks like theres still some config to setup in ./grafana/custom.ini"
+	exit 1
+fi
+
+ln -sf ./grafana/custom.ini /usr/share/grafana/conf/custom.ini
+
---
+++ install_proxy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+sed -i s/Listen 80/#Listen 80/g /etc/apache2/ports.conf
+
+apt-get install -y nginx
+
+ln -fs ./nginx/grafana.conf /etc/nginx/sites-enabled/grafana.conf
+
